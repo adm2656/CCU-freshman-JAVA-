a46c752fe06af31fd3b39bcb710e5f50cdf67e3b
HanselHu <apologize0616@gmail.com>
2016-03-09 00:20:19 +0800

    幹的好!

--- src/RectangleTester.java
+++ src/RectangleTester.java
@@ -1,3 +1,13 @@
+/*
+ * [A]101
+ * [TA's advise]
+ * 1.Rectangle拼錯了(Recyangle), 不影響程式運行, 以後要多注意喔!
+ * 2.加入try-catch的方式非常好, 這應該是目前少數有看到的, extra bouns +1
+ * 3.加入if-else的條件式也是非常好, 不過這部分不夠精確(你只有判斷他是否為0, 但負值呢?), 另外小建議是或許你可以在輸入後馬上檢查是否有問題, 若有問題的話直接丟出例外
+ * 丟出例外的方法throw(),可以研究看看喔!
+ * 4.Good Job
+ * */
+
 import java.util.Scanner;
 
 class Rectangle{
